83bb20f5ab49a85dae377bf858aaee2783557f23
Maaz123k <kmaaz8128@gmail.com>
2026-05-12 11:45:17 +0500

    fix railway deploy

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Backend is working!");
+  res.send("Backend is working");
 });
 
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);    
 });
